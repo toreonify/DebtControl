5022af96f23edaaa0e797e0914d9d2e03d3390dd
toreonify <toreonify@outlook.com>
2022-04-15 13:23:21 +0300

    Initial commit

---
+++ main.pas
@@ -0,0 +1,35 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, DBGrids, Menus,
+  ComCtrls, DefaultTranslator, LCLTranslator;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    DBGrid: TDBGrid;
+    MainMenu: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    StatusBar: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
